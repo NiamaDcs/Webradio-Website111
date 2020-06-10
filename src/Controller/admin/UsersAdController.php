bd63c216a4f22c52e4aa4f3d52272ecbb59f8579
Maysoon <maysoondoucanse@gmail.com>
2020-06-10 10:07:27 +0200

    l'affichage, la modification et la suprression coté admin

--- src/Controller/admin/UsersAdController.php
+++ src/Controller/admin/UsersAdController.php
@@ -20,110 +20,25 @@ class UsersAdController extends BaseController
     {
         return $this->render('admin/user/index.html.twig');
     }
-
+       
     /**
-    * @Route("/show", name="admin.users.show", methods={"POST"})
+    * @Route("/new", name="admin.users.new", methods={"GET"})
     */
-    public function allUser(Request $request)
-    {
-        if($request->isXmlHttpRequest()){
-            $content = $request->getContent();
-
-            $params = json_decode($content, true);
-
-            $token = $params['token'];
-
-            $headers = array('Accept' => 'application/json', 'Authorization' => "Bearer $token");
-            
-            
-            $url = "https://webradio-stream.herokuapp.com/authorized/users";
- 
-            $response = Unirest\Request::get($url,$headers);
- 
-            
-            $result = $response->raw_body; 
-            return new Response($result, 201);
-            
-        }
-    }
-
-    /**
-    * @Route("/new", name="admin.users.new", methods={"GET","POST"})
-    */
-    public function new(Request $request)
+    public function new()
     {
         return $this->render('admin/user/new/new.html.twig');
     }
 
+
     /**
-     * @Route("/edit/{id}", name="admin.users.edit", methods={"GET","POST"})
+     * @Route("/edit/{id}", name="admin.users.edit", methods={"GET"})
      */
-    public function edit(Request $request)
-    {
-        $content = $request->getContent();
-        dump($content);
-        
+    public function edit()
+    { 
         return $this->render('admin/user/edit/edit.html.twig');
     }
 
-    public function getOneUser(Request $request) 
-    {
-        if($request->isXmlHttpRequest()){
-            $content = $request->getContent();
-
-            $params = json_decode($content, true);
-            $token = $params['token'];
-            $idUser = $params['user_id'];
-
-            $headers = array('Accept' => 'application/json', 'Authorization' => "Bearer $token");
-            
-            $url = "https://webradio-stream.herokuapp.com/authorized/users/$idUser";
- 
-            $response = Unirest\Request::get($url,$headers);
- 
-       
-            $result = $response->raw_body; 
-            return new Response($result, 201);
-            
-        }
-    }
-
-    /**
-     * 
-     *@Route("/new", name="admin.users.new", methods={"GET", "POST"})
-     * @param Request $request
-     * @return void
-     */
-    public function createUser(Request $request)
-    {
-        if($request->isMethod("POST")){
-
-            $content = $request->getContent();
-
-            $params = json_decode($content, true);
-
-
-            $email = $params['email'];
-            $username = $params['username'];
-            $token = $params['token'];
-            $avatar = $params['avatar'];
-
-            $headers = array('Accept' => 'application/json', 'Authorization' => "Bearer $token");
-            $query = array('email' => $email, 'username' => $username, 'avatar' =>$avatar);
-            
-            $url = 'https://webradio-stream.herokuapp.com/authorized/users/create';
-            $body = Unirest\Request\Body::form($query);
- 
-            $response = Unirest\Request::post($url,$headers,$body);
- 
-           $result = $response->raw_body;
-
-            return new Response($result, 201);
-            //return new Response(json_encode($query), 201);
-        }
-
-        return $this->render("admin/user/new/new.html.twig");
-    }
+  
 
     
 
